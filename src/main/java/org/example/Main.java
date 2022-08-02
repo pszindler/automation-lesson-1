3073e964457cadb9145e1ebde8adf210dbaee87f
Patryk <pszindler@sii.pl>
2022-08-02 23:04:54 +0200

    little refactor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,6 @@ public class Main {
         String startGameCommunicate = "Please guess the number from 0 to 99.";
         String numberToHighCommunicate = "Your number is GREATER than the one you are trying to guess";
         String numberToLowCommunicate = "Your number is LOWER than the one you are trying to guess";
-        String tryAgainCommunicate = "Please try again";
         String winnerCommunicate = "You guessed it!";
         String losserCommunicate = "Sorry you didn't guess the number, the answer was: ";
         int numberToGuess = new Random().nextInt(100);
@@ -21,24 +20,21 @@ public class Main {
         while (trialGuessing != 0) {
             userNumber = scanner.nextInt();
             if (userNumber > numberToGuess) {
-                trialGuessing = subtractThePointsAndWriteOutTheMessage(numberToLowCommunicate,
-                        tryAgainCommunicate, trialGuessing);
+                trialGuessing = subtractThePointsAndWriteOutTheMessage(numberToLowCommunicate, trialGuessing);
             } else if (userNumber < numberToGuess) {
-                trialGuessing = subtractThePointsAndWriteOutTheMessage(numberToHighCommunicate,
-                        tryAgainCommunicate, trialGuessing);
+                trialGuessing = subtractThePointsAndWriteOutTheMessage(numberToHighCommunicate, trialGuessing);
             } else {
                 System.out.println(winnerCommunicate);
             }
         }
         System.out.println(losserCommunicate + numberToGuess);
     }
-
-    public static int subtractThePointsAndWriteOutTheMessage(String message1, String message2, int tries) {
+    public static int subtractThePointsAndWriteOutTheMessage(String message1, int tries) {
         tries--;
         System.out.println(message1);
         System.out.println("You have " + tries + " more tries.");
         if (tries > 0) {
-            System.out.println(message2);
+            System.out.println("Please try again");
         }
         return tries;
     }
