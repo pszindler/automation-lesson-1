78308a1d340da87c30870ffc54f0a7a97d1f07ad
Patryk <pszindler@sii.pl>
2022-08-03 10:20:48 +0200

    added method, clean main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,17 +5,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        startTheGame();
+    }
 
+    public static int subtractThePointsAndWriteOutTheMessage(String message1, int tries) {
+        tries--;
+        System.out.println(message1);
+        System.out.println("You have " + tries + " more tries.");
+        if (tries > 0) {
+            System.out.println("Please try again");
+        }
+        return tries;
+    }
+
+    public static void startTheGame() {
         String startGameCommunicate = "Please guess the number from 0 to 99.";
         String numberToHighCommunicate = "Your number is GREATER than the one you are trying to guess";
         String numberToLowCommunicate = "Your number is LOWER than the one you are trying to guess";
         String winnerCommunicate = "You guessed it!";
-        String losserCommunicate = "Sorry you didn't guess the number, the answer was: ";
+        String loserCommunicate = "Sorry you didn't guess the number, the answer was: ";
+
         int numberToGuess = new Random().nextInt(100);
         Scanner scanner = new Scanner(System.in);
         int userNumber;
         int trialGuessing = 5;
-
         System.out.println(startGameCommunicate);
         while (trialGuessing != 0) {
             userNumber = scanner.nextInt();
@@ -27,15 +40,6 @@ public class Main {
                 System.out.println(winnerCommunicate);
             }
         }
-        System.out.println(losserCommunicate + numberToGuess);
-    }
-    public static int subtractThePointsAndWriteOutTheMessage(String message1, int tries) {
-        tries--;
-        System.out.println(message1);
-        System.out.println("You have " + tries + " more tries.");
-        if (tries > 0) {
-            System.out.println("Please try again");
-        }
-        return tries;
+        System.out.println(loserCommunicate + numberToGuess);
     }
 }
